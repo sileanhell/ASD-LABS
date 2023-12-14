7d0509514035277afb8108ffdfd391f7f2f77c72
CARZI <146330319+sileanhell@users.noreply.github.com>
2023-12-14 12:03:03 +0200

    new capacity fix & add comments

--- Lab 4/dynamicArray.cpp
+++ Lab 4/dynamicArray.cpp
@@ -4,26 +4,30 @@
 
 DynamicArray createDynamicArrayWithCapacity(int capacity)
 {
+    // Создаем новый DynamicArray с заданной вместимостью
     return { new int[capacity], 0, capacity };
 }
 
 DynamicArray createDynamicArray()
 {
+    // Создаем новый DynamicArray с дефолтной вместимостью 4
     return createDynamicArrayWithCapacity(4);
 }
 
 void addElementToArray(DynamicArray* arr, int element)
 {
+    // Если массив полон, увеличиваем его вместимость в два раза
     if (arr->length == arr->capacity)
     {
-        int newCapacity = std::max(1, arr->capacity * 2);
+        int newCapacity = (arr->capacity == 0) ? 1 : arr->capacity * 2;
         int* newData = new int[newCapacity];
         std::memcpy(newData, arr->data, arr->capacity * sizeof(int));
         delete[] arr->data;
         arr->data = newData;
-        arr->capacity *= 2;
+        arr->capacity = newCapacity;
     }
 
+    // Добавляем новый элемент в конец массива
     arr->data[arr->length++] = element;
 }
 
@@ -35,6 +39,7 @@ int getElementAtIndex(DynamicArray* arr, int index)
 
 std::span<int> getCurrentSpan(DynamicArray* arr)
 {
+    // Возвращаем текущий кусок массива как std::span
     return { arr->data, arr->length };
 }
 
